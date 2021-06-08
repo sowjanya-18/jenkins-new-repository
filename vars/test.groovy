a5e9562da0d7c94db6a456e3faf1b62fdcb5aa26
sowjanya-18 <85096421+sowjanya-18@users.noreply.github.com>
2021-06-08 16:00:27 +0800

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -6,3 +6,19 @@ def add(value1,value2) {
          echo "Displaying value of addition $VALUE"
         '''
     }
+
+
+def multiply(value1,value2) {
+        env.VALUE1 = value1 
+        env.VALUE2 = value2 
+        sh '''
+         VALUE=$(($VALUE1*$VALUE2))
+         echo "Displaying value of Multiplication $VALUE"
+        '''
+    }
+
+def hello() {
+        sh '''
+         echo "say hello to everyone" > hello.txt
+        '''
+    }
