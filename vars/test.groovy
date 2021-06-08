5af481ab9d4ac835c113e673acf78c3ededbe264
sowjanya-18 <85096421+sowjanya-18@users.noreply.github.com>
2021-06-08 15:57:38 +0800

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,7 +2,7 @@ def add(value1,value2) {
         env.VALUE1 = value1 
         env.VALUE2 = value2 
         sh '''
-         VALUE=$($VALUE1+$VALUE2)
+         VALUE=$(($VALUE1+$VALUE2))
          echo "Displaying value of addition $VALUE"
         '''
     }
