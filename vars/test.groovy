b479b583ce54daeb3fd42e1d58da661fc9cfca21
sowjanya-18 <yasamsowji1@gmail.com>
2021-06-08 15:45:32 +0800

    adding shared lib files

---
+++ vars/test.groovy
@@ -0,0 +1,8 @@
+def ad(valu1,value2) {
+        env.VALUE1 = value1 
+        env.VALUE2 = value2 
+        sh '''
+         VALUE=$($VALUE1+$VALUE2)
+         echo "Displaying value of addition $VALUE"
+        '''
+    }
