6abb226a1c4ace1095d8b69132599a3f78a5c791
sowjanya-18 <85096421+sowjanya-18@users.noreply.github.com>
2021-06-09 15:20:15 +0800

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -21,7 +21,7 @@ def subsraction(value1,value2) {
         env.VALUE1 = value1
         env.VALUE2 = value2
         sh '''
-         VALUE=$(($VALUE1+$VALUE2)) 
+         VALUE=$(($VALUE1-$VALUE2)) 
         echo "Displaying value of substarction of $VALUE1 and $VALUE2 IS $VALUE
     }
 
